9a41dfd1d42544726b8d66a3dc750cabc8990586
veronicalee7 <keenkumquat@gmail.com>
2025-03-14 10:54:29 -0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1-ypX1TjaLKM1Q6Hn2KQgqY9puoRMQaCn'
+export_file_url = 'https://www.googleapis.com/drive/v3/files/1-ypX1TjaLKM1Q6Hn2KQgqY9puoRMQaCn?alt=media&key=AIzaSyAPovd14JQlfTrI7OkRkX2nrVPpj0sDk5k'
 export_file_name = 'export.pkl'
 
 classes = ['American craftsman style',
